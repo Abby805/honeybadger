346608267fa248d6dbe7b531e0feb06744e8bff2
Abby Milberg <abby.milberg@bixal.com>
2018-09-17 14:05:02 -0400

    Getting rid of susy

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,15 @@ var fontName = 'icons';
 
 gulp.task('scss', function() {
   return gulp.src('scss/styles.scss')
-    // .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: ['node_modules/susy/sass']
-    }))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
     .on('error', notify.onError({
       title:    "Gulp",
       subtitle: "Failure!",
       message:  "Error: <%= error.message %>"
     }))
     .pipe(cssnano({zindex: false}))
-    // .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write())
     .pipe(postcss([ prefix({ browsers: ['last 2 versions'], cascade: false }) ]))
     .pipe(gulp.dest('css'));
 });
